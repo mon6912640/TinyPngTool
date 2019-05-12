9a2ad9dc5b366717a6d07339612aecdf41624fb5
monkey.lu <010monkey@163.com>
2019-05-12 20:50:08 +0800

    增加本地缓存数据库功能，已经缓存了的文件直接从本地缓存库中获取，不走网络压缩流程

--- TinyPngTool.py
+++ TinyPngTool.py
@@ -7,11 +7,16 @@
 import os.path
 import time
 import argparse
+import hashlib
+import time
+import sqlite3
+import shutil
 
 import tinify
 
-fromPath = "./source"  # source dir path
-toPath = "./compress"  # compress dir path
+from_path = "./source"  # source dir path
+to_path = "./compress"  # compress dir path
+cache_path = './cache'
 
 # 压缩图片的key
 online_key_list = [
@@ -69,39 +74,122 @@ def compress_online(source_path, output_path):
     return result
 
 
+def cal_md5(file_path) -> str:
+    with open(file_path, 'rb') as f:
+        hash = hashlib.md5()
+        while 1:
+            b = f.read(128)
+            if not b:
+                break
+            hash.update(b)
+    md5_str = hash.hexdigest()
+    return md5_str
+
+
+def create_db():
+    # 创建表名为md5的数据库表
+    global conn
+    conn = sqlite3.connect('map.db')
+    global cr
+    cr = conn.cursor()
+    # 建表需要判断数据表是否存在 if not exists
+    cr.execute('create table if not exists `md5` (id text primary key, md5_value text)')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='帮助信息')
-    parser.add_argument('--source', type=str, default=fromPath, help='需要压缩的源目录')
-    parser.add_argument('--output', type=str, default=toPath, help='输出的目录')
+    parser.add_argument('--source', type=str, default=from_path, help='需要压缩的源目录')
+    parser.add_argument('--output', type=str, default=to_path, help='输出的目录')
     args = parser.parse_args()
 
-    print('source_path = '+args.source)
-    print('output_path = '+args.output)
-    fromPath = args.source
-    toPath = args.output
+    print('source_path = ' + args.source)
+    print('output_path = ' + args.output)
+    from_path = args.source
+    to_path = args.output
 
-    if not os.path.exists(fromPath):
+    file_count = 0
+    start = time.time()
+
+    if not os.path.exists(from_path):
         print('源目录不存在，程序退出')
         exit()
 
-    for root, dirs, files in os.walk(fromPath):
-        newToPath = toPath
-        from_abs_path = os.path.abspath(fromPath)
-        to_abs_path = os.path.abspath(toPath)
+    # 创建缓存目录
+    if not os.path.exists(cache_path):
+        os.makedirs(cache_path)
+
+    create_db()  # 创建数据库
+
+    for root, dirs, files in os.walk(from_path):
+        newToPath = to_path
+        from_abs_path = os.path.abspath(from_path)
+        to_abs_path = os.path.abspath(to_path)
 
         for file_name in files:
             source_path = os.path.abspath(os.path.join(root, file_name))
             rel_path = os.path.relpath(source_path, from_abs_path)
             target_path = os.path.join(to_abs_path, rel_path)
-            fileName, fileSuffix = os.path.splitext(file_name)  # 分解文件名的扩展名
-            if fileSuffix == '.png' or fileSuffix == '.jpg':
+            fileName, ext = os.path.splitext(file_name)  # 分解文件名的扩展名
+            if ext == '.png' or ext == '.jpg':
                 # 在线压缩
                 parent_dir = os.path.dirname(target_path)
                 if not os.path.exists(parent_dir):
                     os.makedirs(parent_dir)
-                if not compress_online(source_path, target_path):
-                    print("压缩失败，检查报错信息")
-                    exit()
-                pass
+                # if not compress_online(source_path, target_path):
+                #     print("压缩失败，检查报错信息")
+                #     exit()
+                source_md5 = cal_md5(source_path)
+                key_str = source_md5
+
+                need_upload = False
+                is_in_db = False
+                cr.execute('select * from md5 where id=?', (key_str,))
+                values = cr.fetchone()
+                if values is None:  # 数据库不存在，则需要上传压缩图片
+                    need_upload = True
+                else:  # 数据库存在
+                    is_in_db = True
+                    output_md5 = values[1]
+                    cache_file = os.path.join(cache_path, output_md5 + ext)
+                    if os.path.exists(cache_file):  # 存在缓存文件，则从缓存文件夹复制出来
+                        # 从缓存目录取出来并复制到目标目录
+                        shutil.copyfile(cache_file, target_path)
+                        print(source_path + ' 从缓存取出')
+                        pass
+                    else:  # 不存在缓存文件，则上传压缩图片
+                        need_upload = True
+                        print('数据库存在，但没有缓存 ' + source_path)
+                        pass
+
+                if need_upload:
+                    if not compress_online(source_path, target_path):
+                        print("压缩失败，检查报错信息")
+                        exit()
+                    else:
+                        # 上传压缩成功并下载压缩文件回来了
+                        output_md5 = cal_md5(target_path)
+                        cache_file = os.path.join(cache_path, output_md5 + ext)
+                        shutil.copyfile(target_path, cache_file)  # 复制一份文件到缓存目录
+
+                        if is_in_db:
+                            # 原来数据库有记录，则更新数据库
+                            print('db update ' + output_md5)
+                            cr.execute('update md5 set md5_value=? where id=?', (output_md5, source_md5))
+                            pass
+                        else:
+                            # 数据库没有记录，这插入新数据
+                            print('db insert ' + output_md5)
+                            cr.execute('insert into md5 (id, md5_value) values(?,?)', (source_md5, output_md5))
+                            pass
+
+                file_count += 1
             else:
                 pass
+
+    end = time.time()
+    print('输出 %s 个文件' % file_count)
+    print('总用时', end - start)
+
+    conn.commit()
+    cr.close()
+    conn.close()
--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001
 cd ./
-python TinyPngTool.py --source D:/work/trunk/sanguoclient/resource/assets/image/topIcon --output D:/work/trunk/sanguoclient/resource/assets/image/topIcon/compress
+python TinyPngTool.py --source D:/work/trunk/sanguoclient/resource/assets --output D:/work/trunk/sanguoclient/resource/assets/compress
